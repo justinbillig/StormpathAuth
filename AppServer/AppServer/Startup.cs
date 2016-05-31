311d3421ce3a57b454b8e73bea318a81f07130a3
Stuart Hallows <stuart.hallows@gmail.com>
2016-05-31 12:46:50 +0930

    add appserver2

--- AppServer/AppServer/Startup.cs
+++ AppServer/AppServer/Startup.cs
@@ -1,17 +1,36 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.Owin;
-using Owin;
+﻿//using System;
+//using System.Threading.Tasks;
+//using Microsoft.Owin;
+//using Owin;
 
-[assembly: OwinStartup(typeof(AppServer.Startup1))]
+//[assembly: OwinStartup(typeof(AppServer.Startup))]
 
-namespace AppServer
-{
-    public class Startup1
-    {
-        public void Configuration(IAppBuilder app)
-        {
-            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
-        }
-    }
-}
+//namespace AppServer
+//{
+//    public class Startup
+//    {
+//        public void Configuration(IAppBuilder app)
+//        {
+//            //var myConfiguration = new StormpathConfiguration
+//            //{
+//            //    Application = new ApplicationConfiguration
+//            //    {
+//            //        Href = "your-application-href"
+//            //    }
+//            //};
+
+
+//            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+//            app.UseStormpath();
+
+//            //app.UseStormpath(new StormpathConfiguration()
+//            //{
+//            //    Application = new ApplicationConfiguration()
+//            //    {
+//            //        Name = "My Application"
+//            //    }
+//            //});
+
+//        }
+//    }
+//}
